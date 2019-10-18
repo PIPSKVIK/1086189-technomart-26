f1a0251f2978edf5808cefb90b31ee6cdf89f8ad
Alexander <milovanovska@yandex.ru>
2019-10-18 23:01:08 +0300

    добавил локальный js фаил

---
+++ js/script.js
@@ -0,0 +1,21 @@
+    
+
+/*Первая мадалка index.html Регистрация...*/
+
+var registration = document.querySelector(".registration");
+var popup = document.querySelector(".popup-feedback");
+var close = document.querySelector(".closed");
+
+registration.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log("Клик по ссылке");
+  popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log("Закрыть модальное окно");
+  popup.classList.remove("modal-show");
+})
+
+/*Первая мадалка index.html Регистрация!!!*/
