62a12e59488f36b2c258e6fc63fc42bff1996e0f
Alexander <milovanovska@yandex.ru>
2019-11-02 15:57:58 +0300

    Первый этап отправки, после правок защиты.

--- js/catalog_script.js
+++ js/catalog_script.js
@@ -1,31 +1,28 @@
+/*КУПИТЬ, ТОВАР ДОБАВЛЕН В КОРЗИНУ!*/
+var catalogBuyList = document.querySelectorAll(".catalog-buy");
+var popupBasket = document.querySelector(".popup-item-basket");
+var closeItem = document.querySelector(".closing-form-item");
 
-                  /*КУПИТЬ, ТОВАР ДОБАВЛЕН В КОРЗИНУ!*/
+for (var i = 0; i < catalogBuyList.length; i++) {
+  catalogBuyList[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log("Клик по кнопке КУПИТЬ");
+    popupBasket.classList.add("popup-item-basket-show");
+  });
+}
 
-      var catalogBuyList = document.querySelectorAll(".catalog-buy");
-      var popupBasket = document.querySelector(".popup-item-basket");
-      var closeItem = document.querySelector(".closing-form-item");
+closeItem.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log("Закрыть окно, Товар добавлен в корзину");
+  popupBasket.classList.remove("popup-item-basket-show");
+})
 
-      for (var i = 0; i < catalogBuyList.length; i++) {
-        catalogBuyList[i].addEventListener("click", function (evt) {
-          evt.preventDefault();
-          console.log("Клик по кнопке КУПИТЬ");
-          popupBasket.classList.add("popup-item-basket-show");
-        });
-      }
-
-      closeItem.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        console.log("Закрыть окно, Товар добавлен в корзину");
-        popupBasket.classList.remove("popup-item-basket-show");
-      })
-
-       /*закрываем корзину кнопкой esc*/
-       window.addEventListener("keydown", function (evt) {
-        if (evt.keyCode === 27) {
-          if (popupBasket.classList.contains("popup-item-basket-show")) {
-            evt.preventDefault();
-            popupBasket.classList.remove("popup-item-basket-show");
-            console.log("закрываем окно esc");
-          }
-        }
-       });
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popupBasket.classList.contains("popup-item-basket-show")) {
+      evt.preventDefault();
+      popupBasket.classList.remove("popup-item-basket-show");
+      console.log("закрываем окно esc");
+    }
+  }
+});
--- js/script.js
+++ js/script.js
@@ -1,111 +1,101 @@
-                      
-                      /*Скрипты для первой страници*/
+/*КНОПКА РЕГИСТРАЦИИ В HEADER*/
+var registration = document.querySelector(".button-navigation-info");
+var popup = document.querySelector(".popup-feedback");
+var close = document.querySelector(".closed");
+var login = popup.querySelector("[id=name]");
+var form = popup.querySelector(".form-feedback");
+var email = popup.querySelector("[id=email]");
+var textLetter = popup.querySelector("[id=text-letter]");
 
-                      /*КНОПКА РЕГИСТРАЦИИ В HEADER*/
-      var registration = document.querySelector(".button-navigation-info");
-      var popup = document.querySelector(".popup-feedback");
-      var close = document.querySelector(".closed");
-      var login = popup.querySelector("[id=name]");
-      var form = popup.querySelector(".form-feedback");
-      var email = popup.querySelector("[id=email]");
-      var textLetter = popup.querySelector("[id=text-letter]");
+registration.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log("Клик по ссылке");
+  popup.classList.add("modal-show");
+  login.focus();
+});
 
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log("Закрыть модальное окно");
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
 
-        registration.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        console.log("Клик по ссылке");
-        popup.classList.add("modal-show");
-        login.focus();
-      });
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  console.log(login.value);
+  console.log(email.value);
+  console.log(textLetter.value);
+  if (!login.value || email.value || textLetter.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+  } else {
+    localStorage.setItem("login", login.value);
+  }
+});
 
-      close.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        console.log("Закрыть модальное окно");
-        popup.classList.remove("modal-show");
-        popup.classList.remove("modal-error");
-      });
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+      console.log("Нажали esc, чтобы закрыть окно popup");
+    }
+  }
+});
 
+/*БОЛЬШАЯ КАРТА*/
+var link = document.querySelector(".map-location"); 
+var popupBigMap = document.querySelector(".popup-map");
+var closeBigMap = document.querySelector(".closing-map");
 
-      form.addEventListener("submit", function (evt) {
-        evt.preventDefault();
-        console.log(login.value);   /*Прописываем значение по нашим атрибутам, функции.*/
-        console.log(email.value);
-        console.log(textLetter.value);
-        /*Теперь делаем условие*/
-        if (!login.value || email.value || textLetter.value) {
-          evt.preventDefault();
-          popup.classList.add("modal-error");
-        } else {
-          localStorage.setItem("login", login.value);
-        }
-      });
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+    console.log("Клик по кнопке, Напишите нам!");
+  popupBigMap.classList.add("popup-map-show");
+});
 
-                 /*Закрываем окно popup клавишей esc*/
-      window.addEventListener("keydown", function (evt) {
-        if (evt.keyCode === 27) {
-          if (popup.classList.contains("modal-show")) {
-            evt.preventDefault();
-            popup.classList.remove("modal-show");
-            console.log("Нажали esc, чтобы закрыть окно popup");
-          }
-        }
-      });
-                      /*БОЛЬШАЯ КАРТА*/
-      var link = document.querySelector(".map-location"); 
-      var popupBigMap = document.querySelector(".popup-map");
-      var closeBigMap = document.querySelector(".closing-map");
+closeBigMap.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log("Закрыть модальное большой карты");
+  popupBigMap.classList.remove("popup-map-show");
+});
 
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popupBigMap.classList.contains("popup-map-show")) {
+      evt.preventDefault();
+      popupBigMap.classList.remove("popup-map-show");
+      console.log("нажили esc, чтобы закрыть карту");
+    }
+  }
+});
 
-      link.addEventListener("click", function (evt) {
-        evt.preventDefault();
-          console.log("Клик по кнопке, Напишите нам!");
-        popupBigMap.classList.add("popup-map-show");
-      });
+/*КУПИТЬ, ТОВАР ДОБАВЛЕН В КОРЗИНУ!*/
+var catalogBuyList = document.querySelectorAll(".catalog-buy");
+var popupBasket = document.querySelector(".popup-item-basket");
+var closeItem = document.querySelector(".closing-form-item");
 
-      closeBigMap.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        console.log("Закрыть модальное большой карты");
-        popupBigMap.classList.remove("popup-map-show");
-      });
+for (var i = 0; i < catalogBuyList.length; i++) {
+  catalogBuyList[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log("Клик по кнопке КУПИТЬ");
+    popupBasket.classList.add("popup-item-basket-show");
+  });
+}
 
-        /*закрываем карту кнопкой esc*/
-      window.addEventListener("keydown", function (evt) {
-        if (evt.keyCode === 27) {
-          if (popupBigMap.classList.contains("popup-map-show")) {
-            evt.preventDefault();
-            popupBigMap.classList.remove("popup-map-show");
-            console.log("нажили esc, чтобы закрыть карту");
-          }
-        }
-      });
+closeItem.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log("Закрыть окно, Товар добавлен в корзину");
+  popupBasket.classList.remove("popup-item-basket-show");
+})
 
-                  /*КУПИТЬ, ТОВАР ДОБАВЛЕН В КОРЗИНУ!*/
-
-      var catalogBuyList = document.querySelectorAll(".catalog-buy");
-      var popupBasket = document.querySelector(".popup-item-basket");
-      var closeItem = document.querySelector(".closing-form-item");
-
-      for (var i = 0; i < catalogBuyList.length; i++) {
-        catalogBuyList[i].addEventListener("click", function (evt) {
-          evt.preventDefault();
-          console.log("Клик по кнопке КУПИТЬ");
-          popupBasket.classList.add("popup-item-basket-show");
-        });
-      }
-
-      closeItem.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        console.log("Закрыть окно, Товар добавлен в корзину");
-        popupBasket.classList.remove("popup-item-basket-show");
-      })
-
-       /*закрываем корзину кнопкой esc*/
-       window.addEventListener("keydown", function (evt) {
-        if (evt.keyCode === 27) {
-          if (popupBasket.classList.contains("popup-item-basket-show")) {
-            evt.preventDefault();
-            popupBasket.classList.remove("popup-item-basket-show");
-            console.log("закрываем окно esc");
-          }
-        }
-       });
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popupBasket.classList.contains("popup-item-basket-show")) {
+      evt.preventDefault();
+      popupBasket.classList.remove("popup-item-basket-show");
+      console.log("закрываем окно esc");
+    }
+  }
+});
